2c6c126f66397bdc2e53b5372f75a6e8d5a8184c
Waryono <56479765+WA4YON9-ID-cyber@users.noreply.github.com>
2020-02-07 17:08:54 +0700

    Add files via upload

---
+++ setting.php
@@ -0,0 +1,22 @@
+<?php
+
+$t="\n";         
+                                         $br="\033[1;34m";
+$tr="\033[1;36m";
+$ijo="\033[92m";
+$pth="\033[1;37m";
+$pnk="\033[1;35m";
+$red="\033[1;31m";
+$kn="\033[1;33m";
+$st="online";
+
+$banner = "
+${tr}[-] Server${ijo} ${st}
+${br}[Yodo]${tr} Creator${br} WA4YON9-ID${pth} Versi ${ijo}v1.0
+${tr}[-] Info ${ijo}subrek dulu mamank
+${red}[!] Warning !!!
+${kn}[-] Ini adalah progam illegal
+${kn}[-] Resiko sepenuhnya di tanggung pengguna
+
+";
+$stat_yodo =true;
